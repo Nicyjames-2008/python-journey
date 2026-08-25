76db1844244acdabe65c87a55eb184d4ef1bd841
Nissi <animebozs2024@gmail.com>
2026-08-25 22:30:30 +0530

    Organize Day 1 Python programs

--- datatype.py
+++
@@ -1,7 +0,0 @@
-name = "nicy"
-age = 18
-time = 10.33
-
-print(type(name))
-print(type(age))
-print(type(time))   #type used to tell data type
--- firstprogramme.py
+++
@@ -1,2 +0,0 @@
-print("hello world")
-print("hi i am nicy james ","i am starting my python journey")
