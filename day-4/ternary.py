819f3736e2350dcd399cb3f30bb9ae8771dfedce
Nissi <animebozs2024@gmail.com>
2026-08-28 20:55:01 +0530

    day-4 py journey

---
+++ day-4/ternary.py
@@ -0,0 +1,6 @@
+food = input("Food :")
+eat = "Yes" if food == "cake" or food == "jalebi" else "No"
+print(eat)
+
+food = input("Food :")
+print("Eat") if food == "cake" or food == "jalebi" else print("No")
